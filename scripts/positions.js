d37e9ec26d51755ccf4180998125e553284609bb
Davide Saurino <davide.saurino@gmail.com>
2010-11-30 17:53:41 +0100

    saved-positions: added click functionality on list (open in a different page)

--- scripts/positions.js
+++ scripts/positions.js
@@ -37,12 +37,24 @@ var getPositions = function() {
     store.each(function(r){
 	el = r.value;
 	if(el.name !== undefined) {
-	    console.log(el.name);
-	    newText += "<li><a href=\"whereis2.html?id="+el.index+"\" rel=\"external\">" + el.name + "</a></li>";
+//	    newText += "<li><a href=\"whereis2.html?id="+el.index+"\" rel=\"external\">" + el.name + "</a></li>";
+	    newText += "<li id=\""+el.index+"\">" + el.name + "</li>";
 	}
     });
 
     newText += "</ul>";
     $('.ui-listview').html(newText).listview("refresh");
+
+//    lis = $("li").get();
+//    for (var i = 0; i < lis.length; i++) {
+    $("li").click(function() {
+	attr_value = $(this).attr("id");	     
+	window.location.pathname = "/whereis2.html?id="+attr_value;
+	// $.get('whereis4.html', { id: attr_value }, function(data) {
+	// 	$('.content').html(data);
+	// 	console.log("attr value " + attr_value);
+	// });
+    });
+    //    }
 
 }
